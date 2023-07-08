02e20c4a26a342f9a24d767a8f4d15375c02493c
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-07-08 18:00:38 +0200

    styled TinyThoughtsList view

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {
   ScrollView,
   SafeAreaView,
   Button,
+  StatusBar,
+  ActivityIndicator,
 } from "react-native";
 import { unified } from "unified";
 import markdown from "remark-parse";
@@ -12,7 +14,7 @@ import slate from "remark-slate";
 import rehypeParse from "rehype-parse";
 import rehypeRemark from "rehype-remark";
 import remarkStringify from "remark-stringify";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   ApolloClient,
   InMemoryCache,
@@ -27,7 +29,8 @@ import {
   RichToolbar,
 } from "react-native-pell-rich-editor";
 import RenderHtml from "react-native-render-html";
-import { htmlToSlate } from "slate-serializers";
+import { htmlToSlate, payloadHtmlToSlateConfig } from "slate-serializers";
+import { forwardRef } from "react";
 
 const client = new ApolloClient({
   uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cliiu60970diy01t69chc1zqv/master",
@@ -47,37 +50,42 @@ const query = gql`
   }
 `;
 
-const RichTextEditor = ({ initialHtml, setRichTextAst, editMode }) => {
-  const richText = useRef();
-
-  return (
-    <View>
-      <RichEditor
-        ref={richText}
-        androidHardwareAccelerationDisabled={true}
-        style={styles.richTextEditorStyle}
-        initialContentHTML={initialHtml}
-        onChange={(htmlString) => setRichTextAst(htmlToSlate(htmlString))}
-        disabled={!editMode}
-      />
-      {editMode ? (
-        <RichToolbar
-          editor={richText}
-          selectedIconTint="#873c1e"
-          iconTint="#312921"
-          actions={[
-            actions.setBold,
-            actions.insertBulletsList,
-            actions.insertOrderedList,
-            actions.insertLink,
-            actions.blockquote,
-          ]}
-          style={styles.richTextToolbarStyle}
+const RichTextEditor = forwardRef(
+  ({ initialHtml, setRichTextHTML, editMode }, ref) => {
+    return (
+      <View>
+        {editMode ? (
+          <RichToolbar
+            editor={ref}
+            iconSize={20}
+            selectedIconTint="#873c1e"
+            iconTint="white"
+            actions={[
+              actions.setBold,
+              actions.insertBulletsList,
+              actions.insertOrderedList,
+              actions.insertLink,
+              actions.blockquote,
+              actions.redo,
+              actions.undo,
+            ]}
+            style={styles.richTextToolbarStyle}
+          />
+        ) : null}
+        <RichEditor
+          ref={ref}
+          androidHardwareAccelerationDisabled={true}
+          initialContentHTML={initialHtml}
+          onChange={(htmlString) => {
+            setRichTextHTML(htmlString);
+          }}
+          disabled={!editMode}
+          editorStyle={styles.editorStyle}
         />
-      ) : null}
-    </View>
-  );
-};
+      </View>
+    );
+  }
+);
 
 const MUTATION = gql`
   mutation createTinyThought($content: RichTextAST, $id: ID) {
@@ -97,53 +105,69 @@ const PUBLISH_MUTATION = gql`
   }
 `;
 
-const TinyThoughtItem = ({ initialHtml, rawAst, id }) => {
+const TinyThoughtItem = ({ initialHtml, id }) => {
+  const richTextRef = useRef();
   const [editMode, setEditMode] = useState(false);
-  const [richTestSlateAst, setRichTextAst] = useState(null);
-  const [mutateTinyThought, { updateData, updateLoading, updateError }] =
-    useMutation(MUTATION);
-  const [publishTinyThought, { publishData, publishLoading, publishError }] =
-    useMutation(PUBLISH_MUTATION);
-
-  if (updateError) {
-    console.log(JSON.stringify(error, null, 2));
-  }
-
-  if (publishError) {
-    console.log(JSON.stringify(publishError, null, 2));
-  }
+  const [richTextHTML, setRichTextHTML] = useState(initialHtml);
+  const [
+    mutateTinyThought,
+    { data: updateData, loading: updateLoading, error: updateError },
+  ] = useMutation(MUTATION);
+  const [
+    publishTinyThought,
+    { data: publishData, loading: publishLoading, error: publishError },
+  ] = useMutation(PUBLISH_MUTATION);
 
   return (
-    <>
+    <View style={styles.tinyThoughtItem}>
       <RichTextEditor
         initialHtml={initialHtml}
-        setRichTextAst={setRichTextAst}
+        setRichTextHTML={setRichTextHTML}
         editMode={editMode}
+        ref={richTextRef}
       />
 
       {editMode ? (
         <>
-          <Button
-            onPress={() => {
-              publishTinyThought({ variables: { id } });
-            }}
-            title="publish"
-          />
-          <Button
-            onPress={() => {
-              mutateTinyThought({
-                variables: { content: { children: richTestSlateAst }, id },
-              });
-              setEditMode(false);
-            }}
-            title="save"
-          />
-          <Button
-            onPress={() => {
-              setEditMode(false);
-            }}
-            title="cancel"
-          />
+          <View style={styles.buttonsContainer}>
+            <Button
+              onPress={() => {
+                publishTinyThought({ variables: { id } });
+              }}
+              title="publish"
+              disabled={updateLoading || !updateData}
+              color="black"
+            />
+            <Button
+              onPress={() => {
+                mutateTinyThought({
+                  variables: {
+                    content: { children: htmlToSlate(richTextHTML) },
+                    id,
+                  },
+                });
+              }}
+              title="save"
+              disabled={updateLoading}
+              color="black"
+            />
+            <Button
+              onPress={() => {
+                setEditMode(false);
+              }}
+              title="close"
+              disabled={updateLoading}
+              color="black"
+            />
+          </View>
+          {updateLoading || publishLoading ? (
+            <ActivityIndicator size="large" color="white" />
+          ) : null}
+          {updateError || publishError ? (
+            <Text style={styles.errorMsg}>
+              Error while updating or fetching
+            </Text>
+          ) : null}
         </>
       ) : (
         <Button
@@ -151,9 +175,10 @@ const TinyThoughtItem = ({ initialHtml, rawAst, id }) => {
             setEditMode(true);
           }}
           title="edit"
+          color="black"
         />
       )}
-    </>
+    </View>
   );
 };
 
@@ -161,19 +186,17 @@ const TinyThoughtsList = () => {
   const { data, loading } = useQuery(query);
 
   if (loading) {
-    return <Text>Loading....</Text>;
+    return <ActivityIndicator size="large" color="white" />;
   }
   return (
     <ScrollView overScrollMode="never" nestedScrollEnabled={true}>
       {data.tinyThoughts.map((item) => {
         return (
-          <View key={item.id}>
-            <TinyThoughtItem
-              initialHtml={item.content.html}
-              rawAst={item.content.raw}
-              id={item.id}
-            />
-          </View>
+          <TinyThoughtItem
+            initialHtml={item.content.html}
+            id={item.id}
+            key={item.id}
+          />
         );
       })}
     </ScrollView>
@@ -183,6 +206,7 @@ const TinyThoughtsList = () => {
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
       <ApolloProvider client={client}>
         <TinyThoughtsList />
       </ApolloProvider>
@@ -192,37 +216,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    paddingTop: 100,
-  },
-  richTextContainer: {
-    display: "flex",
-    flexDirection: "column-reverse",
-    width: "100%",
-    marginBottom: 10,
+    backgroundColor: "black",
+    height: "100%",
+    padding: 6,
   },
 
-  richTextEditorStyle: {
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-    borderWidth: 1,
-    borderColor: "#ccaf9b",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    fontSize: 20,
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "black",
+    paddingBottom: 12,
+    paddingTop: 12,
+  },
+
+  tinyThoughtItem: {
+    borderColor: "white",
+    borderWidth: 3,
+    padding: 6,
+    borderRadius: 25,
+    marginBottom: 12,
   },
 
   richTextToolbarStyle: {
-    backgroundColor: "#c6c3b3",
-    borderColor: "#c6c3b3",
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    borderWidth: 1,
+    backgroundColor: "black",
+  },
+
+  errorMsg: {
+    color: "white",
+  },
+  editorStyle: {
+    backgroundColor: "black",
+    color: "white",
   },
 });
